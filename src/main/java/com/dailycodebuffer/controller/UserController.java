4711b525d3be1f7136be2455e876c097a59f8e09
itpark <a.m.smyslov@yandex.ru>
2023-05-22 23:33:45 +0300

    Initial commit

---
+++ src/main/java/com/dailycodebuffer/controller/UserController.java
@@ -0,0 +1,62 @@
+package com.dailycodebuffer.controller;
+
+import com.dailycodebuffer.model.User;
+import com.dailycodebuffer.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/users")
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<String> saveUser(@RequestBody User user) {
+        boolean result = userService.saveUser(user);
+        if(result)
+            return ResponseEntity.ok("User Created Successfully!!");
+        else
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    @GetMapping("/getall")
+    public ResponseEntity<List<User>> fetchAllUser() {
+        List<User> users;
+        users = userService.fetchAllUser();
+        return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<User> fetchUserById(@PathVariable("id") Long id) {
+        User user;
+        user = userService.fetchUserById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @DeleteMapping("/user/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable("id") Long id) {
+        boolean result = userService.deleteUser(id);
+        if(result)
+            return ResponseEntity.ok("User deleted Successfully!!");
+        else
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    @PutMapping("/user/{id}")
+    public ResponseEntity<String> updateUser(@PathVariable("id") Long id, @RequestBody User user) {
+        boolean result = userService.updateUser(id,user);
+        if(result)
+            return ResponseEntity.ok("User Updated Successfully!!");
+        else
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+}
